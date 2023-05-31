d6d24980dc80ab6d6ec0df559d4779a151271687
Atanas Irikev <46356794+na4oman@users.noreply.github.com>
2023-05-31 11:16:53 +0300

    Update app.js

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ const userRouter = require('./routes/users')
 const { notEqual } = require('assert')
 
 // Connect mongoose
-// const dbUrl = process.env.DB_URL
-const dbUrl = 'mongodb://localhost:27017/yelp-camp'
+const dbUrl = process.env.DB_URL
+// const dbUrl = 'mongodb://localhost:27017/yelp-camp'
 // const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
 mongoose.connect(dbUrl, {
   useNewUrlParser: true,
